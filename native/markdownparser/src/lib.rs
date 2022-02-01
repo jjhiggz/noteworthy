715bfc6456260a52954f041d04155f40acd487f6
jjhiggz <jonathan.higger@gmail.com>
2022-02-01 08:24:00 -0700

    cannot figure out how to get these tests to work. There is some problem where the user id, despite showing up as created is being returned as null. I'm not sure why it's happening

--- native/markdownparser/src/lib.rs
+++ native/markdownparser/src/lib.rs
@@ -8,10 +8,4 @@ fn parse(markdown_text: String) -> String {
     MarkdownLine::parse_markdown_to_html(markdown_text)
 }
 
-// #[rustler::nif]
-// fn add(a: i64, b: i64) -> i64 {
-//     a + b
-// }
-
-// rustler::init!("Elixir.MarkdownParser", [add]);
 rustler::init!("Elixir.MarkdownParser", [parse]);
